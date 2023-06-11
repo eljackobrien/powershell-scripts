7d5e01568638a855b47dde816c07c9a550eaa2c8
Jack O'Brien <80079563+eljackobrien@users.noreply.github.com>
2023-06-11 17:51:11 +0100

    Update list_large_files.ps1

--- list_large_files.ps1
+++ list_large_files.ps1
@@ -1,9 +1,8 @@
 # List files with a size bigger than that specified in GB, containing specified string
-$folder = "C:/"
+$folder = "C:/Users/"
 $size = 0.1  # GB (so 1e-3 for MB, 1e-6 for KB....)
 
-ls $folder -recurse -erroraction silentlycontinue|
+ls $folder -recurse -file -erroraction silentlycontinue|
 where {$_.length -gt 1073741824*$size} |
-sort length |
-ft fullname, length |
-format-table -wrap
+sort length -descending |  # Display length in MB rounded (down) to 2 decimal places
+format-table -autosize -property @{label="Size (MB)"; expression={ 1e-2*[int](1e-4*$_.length) }}, fullname
